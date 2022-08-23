54426f2c668079236388c863570fa45e558e8fb6
Mariana Montes <54317904+montesmariana@users.noreply.github.com>
2022-08-23 21:32:33 +0200

    split hdbscan data in smaller files

--- generate_data.R
+++ generate_data.R
@@ -5,14 +5,14 @@ library(cli)
 
 lemmas <- dir(here("tokens"))
 
-res <- map(setNames(lemmas, lemmas), function(lemma) {
+walk(lemmas, function(lemma) {
   qlvl_dir <- file.path("../../qlvl/NephoVis/tokenclouds/data/", lemma)
-  models <- read_tsv(sprintf("%s/%s.medoids.tsv", qlvl_dir, lemma), show_col_types = FALSE)$medoid
+  models <- read_tsv(sprintf("%s/%s.medoids.tsv", qlvl_dir, lemma),
+                     show_col_types = FALSE)$medoid
   
   map(setNames(models, models), summarizeHDBSCAN, lemma = lemma,
       input_dir = here("tokens", lemma),
       output_dir = qlvl_dir,
-      coords_name = ".tsne.30")
+      coords_name = ".tsne.30") %>% 
+    saveRDS(here("hdbscan", paste0(lemma, ".rds")))
 })
-
-write_rds(res, here("data.rds"))
